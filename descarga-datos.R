3795b4ae91dd5fb47e397a1ab397e8f702f25a39
joaquingue97 <68792925+joaquingue97@users.noreply.github.com>
2022-09-30 16:50:44 -0300

    correción nuevos datos

--- descarga-datos.R
+++ descarga-datos.R
@@ -1,3 +1,3 @@
 download.file(url = "https://cdn.buenosaires.gob.ar/datosabiertos/datasets/transporte-y-obras-publicas/bicicletas-publicas/recorridos-realizados-2019.zip",
               
-              destfile = "datos/recorridos-realizados-2019.csv")
+              destfile = "datos/recorridos-realizados-2019.zip")
